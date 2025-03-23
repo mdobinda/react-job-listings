fd7c8f02f1a85d015e01af4d51beff5520abe394
min E <66760972+mdobinda@users.noreply.github.com>
2025-03-22 23:03:30 -0400

    routing and adding actual mock server backend

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const MainLayout = () => {
+  return (
+   <>
+   <Navbar/>
+   <Outlet />
+   </>
+  )
+}
+
+export default MainLayout
