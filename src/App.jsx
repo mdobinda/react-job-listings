26e23ccb89d27610cb4f30ed0f7e97684fa16bbe
min E <66760972+mdobinda@users.noreply.github.com>
2025-03-23 00:28:53 -0400

    add and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,47 @@ import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, { jobLoader } from './pages/JobPage'
+import AddJobPage from './pages/AddjJobPage'
 
-const router = createBrowserRouter( 
-  createRoutesFromElements(
-  <Route path="/" element={<MainLayout />}>
-     <Route index element={<HomePage />} />
-     <Route path="jobs" element={<JobsPage />} />
-     <Route path="jobs/:id" element={<JobPage />} loader={jobLoader} />
-     <Route path="*" element={<NotFoundPage />} />
-  
-  </Route>
-  )
- 
-);
+
 
 const App = () => {
+
+  // add new job 
+  const addJob = async(newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+      });
+      return; 
+  }
+
+  // delete job
+  const deleteJob = async(id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+      });
+      return; 
+  }
+
+  
+  const router = createBrowserRouter( 
+    createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+       <Route index element={<HomePage />} />
+       <Route path="jobs" element={<JobsPage />} />
+       <Route path="add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+       <Route path="jobs/:id" element={<JobPage deleteaJob={deleteJob} />} loader={jobLoader} />
+       <Route path="*" element={<NotFoundPage />} />
+    
+    </Route>
+    )
+   
+  );
+
   return <RouterProvider router={router} />
 }
 
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import React from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from '../components/Navbar'
 
 const MainLayout = () => {
@@ -7,6 +9,7 @@ const MainLayout = () => {
    <>
    <Navbar/>
    <Outlet />
+   <ToastContainer />
    </>
   )
 }
