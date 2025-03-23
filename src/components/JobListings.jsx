48bad6aa646d68762972b2a9182d317352fd099c
min E <66760972+mdobinda@users.noreply.github.com>
2025-03-22 23:27:43 -0400

    api and proxy change

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import { FaSpinner } from 'react-icons/fa';
 
 const JobListings = ( {isHome = false }) => {
 
-    const jobListings  = isHome ? jobs.slice(0, 3) : jobs;
+  // this is an empty array. we'll make request when the component loads through useEffect - then fill this state from the response of the api 
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true); // once it fetches the data we can set this to false 
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
     console.log(jobs); 
   return (
@@ -14,11 +34,16 @@ const JobListings = ( {isHome = false }) => {
         { isHome ? 'Recent Jobs' : 'Browse Jobs'}
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {jobListings.map((job) => (
+        { loading ? (<FaSpinner/>) : (
+          <>
+           {jobs.map((job) => (
           
-            <JobListing key={job.id} job={job} />
-            
-        ))}
+          <JobListing key={job.id} job={job} />
+          
+      ))}
+          </>
+        )}
+       
        
       </div>
     </div>
